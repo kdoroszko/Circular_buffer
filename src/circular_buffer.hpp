f1b64a1bf86ff491b2d1054c4ad0f585c4a47191
kdoroszko <doroszko.k@gmail.com>
2020-05-06 22:25:45 +0200

    Add new methods: push and show_elements

--- src/circular_buffer.hpp
+++ src/circular_buffer.hpp
@@ -2,12 +2,34 @@
 
 #include <cstddef>
 #include <deque>
+#include <iostream>
 
 template <typename T>
 class Circular_buffer
 {
 public:
     Circular_buffer(std::size_t new_size) : size_(new_size) {}
+
+    void push(const T new_element)
+    {
+        if(buffer_.size() < size_)
+            buffer_.emplace_back(new_element);
+        else std::cout << "Buffer is full! Can't add any new object!\n";
+    }
+
+    void show_elements()
+    {
+        std::cout << "--- Elements in buffer ---\n";
+
+        if(buffer_.empty())
+            std::cout << "\tBuffer is empty!\n\n";
+        else
+        {
+            for(auto elem: buffer_)
+                std::cout << elem << '\n';
+            std::cout << '\n';
+        }
+    }
 
 private:
     std::deque<T> buffer_;
--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,11 @@ int main()
 {
     Circular_buffer<int> test_buffer(10);
 
+    test_buffer.show_elements();
+    test_buffer.push(1);
+    test_buffer.push(2);
+    test_buffer.push(3);
+    test_buffer.show_elements();
+
     return 0;
 }
